7b64890c79984138d4500dfaef896ab38a6ac4e9
Quang Son <luuquangson1999@gmail.com>
2022-05-05 00:28:03 +0700

    update slider

--- src/component/slide/Slide.jsx
+++ src/component/slide/Slide.jsx
@@ -1,77 +1,49 @@
-import "../slide/Slide.scss";
-import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
 import { useState } from "react";
+import "../slide/Slide.scss";
+import BtnSlice from "./BtnSlice"
+import DataSlice from "./dataSlide";
+
 
 const Slide = () => {
 
-  const [index, setIndex] = useState(0);
+  const [slideIndex, setSlideIndex] = useState(1);
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+  const nextSlide = () => {
+    if (slideIndex !== DataSlice.length) {
+      setSlideIndex(slideIndex + 1)
+    } 
+    else if (slideIndex === DataSlice.length) {
+      setSlideIndex(1)
+    }
+  }
+  const prevSlide = () => {
+    if(slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1)
+    }
+      else if (slideIndex === 1){
+        setSlideIndex(DataSlice.length)
+      }
+  }
   return (
-    <nav div className="carousel">
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kfcvietnam.com.vn/uploads/banner/7d40c83e741b15cff4d2b44b17d06e7c.png"
-          alt="First slide"
-        />
-        <Carousel.Caption>
- 
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kfcvietnam.com.vn/uploads/banner/bb8e434961ce7ce3fca01f834974f9c6.png"
-          alt="Second slide"
-        />
 
-        <Carousel.Caption>
-    
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kfcvietnam.com.vn/uploads/banner/fa2e9c2b85b8228f08cc0efc8559943f.png"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
+    <div className="container-slider">
+      {DataSlice.map((obj, i) => {
+        return (
 
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kfcvietnam.com.vn/uploads/banner/6701abc0f42dcbb7590c3132994e64bf.png"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kfcvietnam.com.vn/uploads/banner/d0d69f34e549f5754b49ce15536d1c25.png"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kfcvietnam.com.vn/uploads/banner/5541d50595d63cf247bd3a2accb0e7f2.png"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    </nav>
+          <div key={obj.id}
+            className={slideIndex === i + 1 ? "slide active-anim" : "slide"}>
+
+            <img src={process.env.PUBLIC_URL + `/dataImage/img${i + 1}.jpg`} />
+          </div>
+        )
+      })
+      }
+      <BtnSlice moveSlide={nextSlide} direction={"next"} />
+      <BtnSlice moveSlide={prevSlide} direction={"prev"} />
+
+    </div>
+
   );
 }
-export default Slide ;
+export default Slide;
